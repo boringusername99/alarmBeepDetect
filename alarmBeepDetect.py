e450e431d81b9de3d48aa3ea9f389209811fc210
Jason Yau <booyau313@gmail.com>
2018-09-08 06:14:04 -0700

    testing, add device enumerator

--- alarmBeepDetect.py
+++ alarmBeepDetect.py
@@ -10,31 +10,40 @@ from time import sleep
 #Volume Sensitivity, 0.05: Extremely Sensitive, may give false alarms
 #             0.1: Probably Ideal volume
 #             1: Poorly sensitive, will only go off for relatively loud
-SENSITIVITY= 1
+SENSITIVITY = .1
+
 # Alarm frequency (Hz) to detect (Set frequencyoutput to True if you need to detect what frequency to use)
-TONE = 3500
+TONE = 1742
+
 #Bandwidth for detection (i.e., detect frequencies +- within this margin of error of the TONE)
 BANDWIDTH = 30
+
 #How many 46ms blips before we declare a beep? (Set frequencyoutput to True if you need to determine how many blips are found, then subtract some)
-beeplength=8
+beeplength=1
+
 # How many beeps before we declare an alarm? (Avoids false alarms)
-alarmlength=5
+alarmlength=10
+
 # How many false 46ms blips before we declare there are no more beeps? (May need to be increased if there are expected long pauses between beeps) 
 resetlength=10
+
 # How many reset counts until we clear an active alarm? (Keep the alarm active even if we don't hear this many beeps)
-clearlength=30
+clearlength=10
+
 # Enable blip, beep, and reset debug output (useful for understanding when blips, beeps, and resets are being found)
-debug=False
+debug=True
+
 # Show the most intense frequency detected (useful for configuration of the frequency and beep lengths)
-frequencyoutput=False
+frequencyoutput=True
 
 
 # Audio Sampler
 NUM_SAMPLES = 2048
 SAMPLING_RATE = 44100
 pa = pyaudio.PyAudio()
-_stream = pa.open(format=pyaudio.paInt16,
-                  channels=1, rate=SAMPLING_RATE,
+_stream = pa.open(input_device_index=0,
+				  format=pyaudio.paInt16,
+                  channels=2, rate=SAMPLING_RATE,
                   input=True,
                   frames_per_buffer=NUM_SAMPLES)
 
@@ -52,7 +61,8 @@ while True:
          _stream.get_read_available()), dtype=short)[-NUM_SAMPLES:]
     # Each data point is a signed 16 bit number, so we can normalize by dividing 32*1024
     normalized_data = audio_data / 32768.0
-    intensity = abs(fft(normalized_data))[:NUM_SAMPLES/2]
+    slice_size = int(NUM_SAMPLES/2)
+    intensity = abs(fft(normalized_data))[:slice_size]
     frequencies = linspace(0.0, float(SAMPLING_RATE)/2, num=NUM_SAMPLES/2)
     if frequencyoutput:
         which = intensity[1:].argmax()+1
@@ -66,34 +76,34 @@ while True:
             thefreq = which*SAMPLING_RATE/NUM_SAMPLES
     if max(intensity[(frequencies < TONE+BANDWIDTH) & (frequencies > TONE-BANDWIDTH )]) > max(intensity[(frequencies < TONE-1000) & (frequencies > TONE-2000)]) + SENSITIVITY:
         if frequencyoutput:
-            print "\t\t\t\tfreq=",thefreq
+            print("\t\t\t\tfreq=",thefreq)
         blipcount+=1
         resetcount=0
-        if debug: print "\t\tBlip",blipcount
+        if debug: print("\t\tBlip",blipcount)
         if (blipcount>=beeplength):
             blipcount=0
             resetcount=0
             beepcount+=1
-            if debug: print "\tBeep",beepcount
+            if debug: print("\tBeep",beepcount)
             if (beepcount>=alarmlength):
                 clearcount=0
                 alarm=True
-                print "Alarm!"
+                print("Alarm!")
                 beepcount=0
     else:
         if frequencyoutput:
-            print "\t\t\t\tfreq="
+            print("\t\t\t\tfreq=", thefreq)
         blipcount=0
         resetcount+=1
-        if debug: print "\t\t\treset",resetcount
+        if debug: print("\t\t\treset",resetcount)
         if (resetcount>=resetlength):
             resetcount=0
             beepcount=0
             if alarm:
                 clearcount+=1
-                if debug: print "\t\tclear",clearcount
+                if debug: print("\t\tclear",clearcount)
                 if clearcount>=clearlength:
                     clearcount=0
-                    print "Cleared alarm!"
+                    print("Cleared alarm!")
                     alarm=False
     sleep(0.01)
